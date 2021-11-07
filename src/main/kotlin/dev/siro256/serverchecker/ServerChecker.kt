24503b92ce0079aad05e9ab3d5df8cf40c6b5287
Siro_256 <siro@siro256.dev>
2021-11-07 22:06:34 +0900

    Add version variable

--- src/main/kotlin/dev/siro256/serverchecker/ServerChecker.kt
+++ src/main/kotlin/dev/siro256/serverchecker/ServerChecker.kt
@@ -3,6 +3,8 @@ package dev.siro256.serverchecker
 import dev.siro256.kotlin.consolelib.Console
 
 object ServerChecker {
+    const val VERSION = "1.0.0-SNAPSHOT"
+
     @JvmStatic
     fun main(args: Array<String>) {
         //Initialize console class
