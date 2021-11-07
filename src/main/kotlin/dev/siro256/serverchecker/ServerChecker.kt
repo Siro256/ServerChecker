0e7c157f4daf542fb116b6333c1993dbfed7c109
Siro_256 <siro@siro256.dev>
2021-11-07 22:05:24 +0900

    Add console initializer

--- src/main/kotlin/dev/siro256/serverchecker/ServerChecker.kt
+++ src/main/kotlin/dev/siro256/serverchecker/ServerChecker.kt
@@ -1,7 +1,11 @@
 package dev.siro256.serverchecker
 
+import dev.siro256.kotlin.consolelib.Console
+
 object ServerChecker {
     @JvmStatic
     fun main(args: Array<String>) {
+        //Initialize console class
+        Console.initialize()
     }
 }
