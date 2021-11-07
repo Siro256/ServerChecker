47b254355643aa02566ceed013b1e97c1b3e47dc
Siro_256 <siro@siro256.dev>
2021-11-07 22:22:39 +0900

    Create MonitorWorker

---
+++ src/main/kotlin/dev/siro256/serverchecker/worker/MonitorWorker.kt
@@ -0,0 +1,15 @@
+package dev.siro256.serverchecker.worker
+
+import dev.siro256.serverchecker.MonitorMethod
+import dev.siro256.serverchecker.data.MonitorTarget
+
+abstract class MonitorWorker {
+    abstract val method: MonitorMethod
+
+    @Throws(IllegalArgumentException::class)
+    abstract fun checkTarget(target: MonitorTarget): Boolean
+
+    override fun toString(): String {
+        return "$method-Worker"
+    }
+}
