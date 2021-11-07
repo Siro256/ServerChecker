403ac50ea1c1a029c97b594fa453ca2400c1d398
Siro_256 <siro@siro256.dev>
2021-11-08 04:20:35 +0900

    Create a HTTP monitoring worker

--- src/main/kotlin/dev/siro256/serverchecker/ServerChecker.kt
+++ src/main/kotlin/dev/siro256/serverchecker/ServerChecker.kt
@@ -2,10 +2,13 @@ package dev.siro256.serverchecker
 
 import dev.siro256.kotlin.consolelib.Console
 import dev.siro256.kotlin.eventlib.EventManager
+import dev.siro256.serverchecker.worker.HttpWorker
+import dev.siro256.serverchecker.worker.MonitorWorker
 
 object ServerChecker {
     const val VERSION = "1.0.0-SNAPSHOT"
 
+    private val monitorWorker = mutableListOf<MonitorWorker>()
     @JvmStatic
     fun main(args: Array<String>) {
         //Initialize console class
@@ -17,5 +20,10 @@ object ServerChecker {
 
         //Register console event
         EventManager.registerHandler(ConsoleListener)
+
+        //Register worker to list
+        listOf(
+            HttpWorker
+        ).forEach { monitorWorker.add(it) }
     }
 }
---
+++ src/main/kotlin/dev/siro256/serverchecker/worker/HttpWorker.kt
@@ -0,0 +1,38 @@
+package dev.siro256.serverchecker.worker
+
+import dev.siro256.serverchecker.ConfigManager
+import dev.siro256.serverchecker._enum.MonitorMethod
+import dev.siro256.serverchecker.ServerChecker
+import dev.siro256.serverchecker.data.MonitorTarget
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.runBlocking
+
+object HttpWorker: MonitorWorker() {
+    override val method = MonitorMethod.HTTP
+
+    private val client = HttpClient(CIO) {
+        expectSuccess = false
+
+        engine {
+            requestTimeout = ConfigManager.timeout.toLong()
+        }
+
+        install(UserAgent) {
+            agent = "ServerChecker/${ ServerChecker.VERSION } HTTP-Worker"
+        }
+    }
+
+    override fun checkTarget(target: MonitorTarget): Boolean  {
+        if (target.method != MonitorMethod.HTTP) throw IllegalArgumentException("Target method must be HTTP.")
+
+        return runBlocking {
+            if ((client.get(target.destination) as HttpResponse).status != HttpStatusCode.OK)
+                return@runBlocking false else true
+        }
+    }
+}
