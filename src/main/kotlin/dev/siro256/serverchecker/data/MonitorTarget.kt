7dcaa939023c97db8f33e7dd19e41a7c7de05536
Siro_256 <siro@siro256.dev>
2021-11-08 04:12:19 +0900

    Add monitoring information in MonitorTarget

--- src/main/kotlin/dev/siro256/serverchecker/data/MonitorTarget.kt
+++ src/main/kotlin/dev/siro256/serverchecker/data/MonitorTarget.kt
@@ -1,11 +1,15 @@
 package dev.siro256.serverchecker.data
 
 import dev.siro256.serverchecker._enum.MonitorMethod
+import dev.siro256.serverchecker._enum.TargetStatus
+import java.time.ZonedDateTime
 
 data class MonitorTarget(
     val name: String,
     val description: String,
     val destination: String,
     val method: MonitorMethod,
-    val period: Int
+    val period: Int,
+    var previousStatus: TargetStatus = TargetStatus.UNCONFIRMED,
+    var lastChangedTime: ZonedDateTime = ZonedDateTime.now()
 )
