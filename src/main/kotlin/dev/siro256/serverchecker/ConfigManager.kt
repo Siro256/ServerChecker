123ac5b99ec532d3c59c345bd2983bc3c6dcc334
Siro_256 <siro@siro256.dev>
2021-11-07 22:20:36 +0900

    Create loadConfig() method

--- src/main/kotlin/dev/siro256/serverchecker/ConfigManager.kt
+++ src/main/kotlin/dev/siro256/serverchecker/ConfigManager.kt
@@ -1,12 +1,33 @@
 package dev.siro256.serverchecker
 
 import dev.siro256.kotlin.consolelib.Console
+import dev.siro256.serverchecker.data.MonitorTarget
 import org.yaml.snakeyaml.Yaml
 import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.system.exitProcess
 
 object ConfigManager {
+    var webhookUrl = ""
+        private set
+
+    var useServer = true
+        private set
+
+    var bindAddress = "0.0.0.0"
+        private set
+
+    var bindPort = 6712
+        private set
+
+    var timeout = 2000
+        private set
+
+    var maxRetry = 3
+        private set
+
+    val target = mutableListOf<MonitorTarget>()
+
     fun deployFile() {
         val destinationFile = Paths.get("./config.yml")
 
@@ -45,4 +66,37 @@ object ConfigManager {
         exitProcess(0)
     }
 
+    fun loadConfig() {
+        val yaml = Yaml()
+
+        yaml.loadAs(Paths.get("./config.yml").toFile().inputStream(), HashMap<String, Any>()::class.java)
+            .forEach { (key, value) ->
+                when (key) {
+                    "webhookUrl" -> webhookUrl = value.toString()
+                    "useServer" -> useServer = value as Boolean
+                    "bindAddress" -> bindAddress = value.toString()
+                    "bindPort" -> bindPort = value as Int
+                    "timeout" -> timeout = value as Int
+                    "maxRetry" -> maxRetry = value as Int
+                    "monitorTarget" -> {
+                        yaml.loadAs(yaml.dump(value), mutableListOf<Map<String, Any>>()::class.java).forEach {
+                            target.add(
+                                MonitorTarget(
+                                    name = it["name"]!!.toString(),
+                                    description = it["description"]!!.toString()
+                                        .let { description -> description.ifEmpty { "(Empty)" } },
+                                    method = when (it["method"]!!.toString()) {
+                                        "HTTP" -> MonitorMethod.HTTP
+                                        "PING" -> MonitorMethod.PING
+                                        else -> MonitorMethod.SERVER_CHECKER
+                                    },
+                                    destination = it["destination"]!!.toString(),
+                                    period = it["period"]!! as Int
+                                )
+                            )
+                        }
+                    }
+                }
+            }
+    }
 }
